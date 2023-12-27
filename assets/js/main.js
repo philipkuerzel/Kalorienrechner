6c544f3356f3e2a3d7718fe1bcfca9f13fb72675
philipkuerzel <philipkuerzel@gmail.com>
2023-12-27 11:55:59 +0100

    html fixed and js done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,22 @@
-const age = document.querySelector("#age").value
-const sex = document.querySelector("#gender").value
-const bodyWeight = document.querySelector("#weight").value
-const bodyHeight = document.querySelector("#height").value
-const pal = document.querySelector("#pal").value
-const result = document.querySelector("#result")
 
 let bmr;
-const calculateBMR = (age, sex, bodyWeight, bodyHeight) => {
-    if (sex === "male") {
-        bmr = 66 + (13.7 * bodyWeight) + (5 * bodyHeight) - (6.8 * age)
-    }
-    else {
-        bmr = 655 + (9.6 * bodyWeight) + (1.8 * bodyHeight) - (4.7 * age)
-    }return bmr
-}
-
 let calories;
 
-const calculateCalories = (bmr, pal) => {
+const calculateCalories = (event) => {
+
+    const age = document.querySelector("#age").value
+    const sex = document.querySelector("#gender").value
+    const bodyWeight = document.querySelector("#weight").value
+    const bodyHeight = document.querySelector("#height").value
+    const pal = document.querySelector("#pal").value
+
+    if (sex === "male") {
+        bmr = 66 + 13.7 * bodyWeight + 5 * bodyHeight - 6.8 * age
+    }
+    else {
+        bmr = 655 + 9.6 * bodyWeight + 1.8 * bodyHeight - 4.7 * age
+    }
     calories = bmr * pal
-    return calories
+    document.querySelector("#resultBMR").textContent = `${bmr.toFixed(0)} kcal`
+    document.querySelector("#resultPal").textContent = `${calories.toFixed(0)} kcal`
 }
-
-
-result.textContent = `Gesamtkalorien:  ${calories} kcal`
